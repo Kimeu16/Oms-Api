21aa9bdf886c26641c11583602eb59b928dfdf46
Tonnie <tkymeu@gmail.com>
2023-06-30 11:10:42 +0300

    updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.eager_load = false
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
