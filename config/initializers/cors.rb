34c7550d5efd9fe63466ac560266f02dda1070c5
Tonnie <tkymeu@gmail.com>
2023-07-01 10:35:37 +0300

    updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins [ 'https://oms-api-production.up.railway.app', 'https://office-management-system.vercel.app']
+    origins [ 'https://oms-api-production.up.railway.app', 'https://office-management-system.vercel.app', 'http://localhost:3000', 'http://localhost:3001']
 
     resource "*",
       headers: :any,
