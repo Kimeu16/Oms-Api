be136599868fcf03aab7a4bed6c1938715acb3ee
Tonnie <tkymeu@gmail.com>
2023-07-10 10:53:19 +0300

    updates

--- app/controllers/leave_calculations_controller.rb
+++ app/controllers/leave_calculations_controller.rb
@@ -1,31 +1,40 @@
 class LeaveCalculationsController < ApplicationController
-
-  # GET /  LeaveCalculations
+  # GET /leave_calculations
   def index
-    @leave_calculation = LeaveCalculation.all
-    render json: @leave_calculation
+    @leave_calculations = LeaveCalculation.all
+    render json: @leave_calculations
   end
 
-  # GET /leave_types/1
+  # GET /leave_calculations/1
   def show
     @leave_calculation = set_leave_calculation
     render json: @leave_calculation
   end
 
-  # POST /leave_types
+  # POST /leave_calculations
   def create
     @leave_calculation = LeaveCalculation.create(leave_calculation_params)
-    render json: @leave_calculation, status: :created
+    update_leave_days(@leave_calculation) # Calculate and update leave days
+    if @leave_calculation.save
+      render json: @leave_calculation, status: :created
+    else
+      render json: @leave_calculation.errors, status: :unprocessable_entity
+    end
   end
 
-  # PATCH/PUT /leave_types/1
+  # PATCH/PUT /leave_calculations/1
   def update
     @leave_calculation = set_leave_calculation
     @leave_calculation.update(leave_calculation_params)
-    render json: @leave_calculation, status: :created
+    update_leave_days(@leave_calculation) # Recalculate and update leave days
+    if @leave_calculation.save
+      render json: @leave_calculation, status: :ok
+    else
+      render json: @leave_calculation.errors, status: :unprocessable_entity
+    end
   end
 
-  # DELETE /leave_types/1
+  # DELETE /leave_calculations/1
   def destroy
     @leave_calculation = set_leave_calculation
     @leave_calculation.destroy
@@ -33,14 +42,20 @@ class LeaveCalculationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_leave_calculation
-      @leave_calculation = LeaveCalculation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def leave_calculation_params
-      params.permit(:staff_details, :type_of_leave, :total_days, :used_days, :available_days, :leave_type_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_leave_calculation
+    @leave_calculation = LeaveCalculation.find(params[:id])
+  end
 
+  # Only allow a list of trusted parameters through.
+  def leave_calculation_params
+    params.permit(:staff_details, :type_of_leave, :total_days, :used_days, :available_days, :leave_type_id)
+  end
+
+  # Update the leave days (used_days, available_days) based on total_days
+  def update_leave_days(leave_calculation)
+    leave_calculation.used_days = leave_calculation.used_days.to_i
+    leave_calculation.available_days = [leave_calculation.total_days.to_i - leave_calculation.used_days.to_i, 0].max
+  end
 end
