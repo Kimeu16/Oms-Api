b23535871335d678af54aec819e9f21ef0328b39
Tonnie <tkymeu@gmail.com>
2023-07-17 15:24:41 +0300

    update

--- app/serializers/form_serializer.rb
+++ app/serializers/form_serializer.rb
@@ -1,5 +1,5 @@
 class FormSerializer < ActiveModel::Serializer
-  attributes :id, :your_name, :date_from, :date_to, :reason_for_leave, :leaving_type, :status, :staff_id
+  attributes :id, :your_name, :date_from, :date_to, :reason_for_leave, :leaving_type, :status, :staff_id, :updated_at, :created_at
 
   belongs_to :staff
 end
