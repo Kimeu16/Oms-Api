439722a034dc23202dd74a925c71ae955e453d96
Tonnie <tkymeu@gmail.com>
2023-08-30 23:46:42 +0300

    fix bugs

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # Devise modules
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  # Custom roles
-  enum role: [:admin, :staff]
-end
