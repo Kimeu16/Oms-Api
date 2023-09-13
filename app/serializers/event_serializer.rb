60db2e3ffb9a945ecb9caf1c92225951f637f009
Tonnie <tkymeu@gmail.com>
2023-09-13 16:22:43 +0300

    events update

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :time,:agenda, :host, :trainer, :documents, :email, :meeting_link, :staff_id, :client_id, :manager_id
+  attributes :id, :date, :time,:agenda, :host, :trainer, :documents, :email, :meeting_link, :staff_id, :client_id, :manager_id
 end
